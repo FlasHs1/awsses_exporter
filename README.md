67e16858f13edc3261ceda40ba7d9bb703f6c626
Eugene Konifatov <evgeniy.konifatov@nurd.com>
2019-01-10 19:00:35 +0200

    modified README, changed collected metrics and it's processing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AWS SES Exporter
 
-Exporter for AWS SES quotas. `awsses_exporter` will poll all AWS SES regions and report back the max send rate, max send per 24 hours, and current send in last 24 hours. The AWS SES API endpoint `GetSendQuota` is called once per region per call to the `/metrics` endpoint
+Exporter for AWS SES send statistics. `awsses_exporter` will poll all AWS SES regions and report back the latest Complaints, Timestamp, DeliveryAttempts, Bounces, Rejects. The AWS SES API endpoint `GetSendStatisticsInput` is called once per region per call to the `/metrics` endpoint
 
 ## Usage
 ```
@@ -27,27 +27,28 @@ This easiest way to build and run this utility is with the included docker image
 
 You can also install it locally with:
 ```
-go get github.com/sysadmind/awsses_exporter
-go install github.com/sysadmind/awsses_exporter
+go get github.com/FlasHs1/awsses_exporter.git
+go install github.com/FlasHs1/awsses_exporter.git
 ```
 
 
 ## Example Metrics
 Below is an example of the output of the `/metrics` endpoint
 ```
-# HELP awsses_exporter_max24hoursend The maximum number of emails allowed to be sent in a rolling 24 hours.
-# TYPE awsses_exporter_max24hoursend gauge
-awsses_exporter_max24hoursend{aws_region="eu-west-1"} 50000
-awsses_exporter_max24hoursend{aws_region="us-east-1"} 50000
-awsses_exporter_max24hoursend{aws_region="us-west-2"} 50000
-# HELP awsses_exporter_maxsendrate The maximum rate of emails allowed to be sent per second.
-# TYPE awsses_exporter_maxsendrate gauge
-awsses_exporter_maxsendrate{aws_region="eu-west-1"} 14
-awsses_exporter_maxsendrate{aws_region="us-east-1"} 14
-awsses_exporter_maxsendrate{aws_region="us-west-2"} 14
-# HELP awsses_exporter_sentlast24hours The number of emails sent in the last 24 hours.
-# TYPE awsses_exporter_sentlast24hours gauge
-awsses_exporter_sentlast24hours{aws_region="eu-west-1"} 101
-awsses_exporter_sentlast24hours{aws_region="us-east-1"} 505
-awsses_exporter_sentlast24hours{aws_region="us-west-2"} 888
+# HELP awsses_exporter_bounces Bounces per region
+# TYPE awsses_exporter_bounces gauge
+awsses_exporter_bounces{aws_region="us-east-1"} 0.0
+awsses_exporter_bounces{aws_region="us-west-2"} 0.0
+# HELP awsses_exporter_complaints Complaints per region
+# TYPE awsses_exporter_complaints gauge
+awsses_exporter_complaints{aws_region="us-east-1"} 0.0
+awsses_exporter_complaints{aws_region="us-west-2"} 0.0
+# HELP awsses_exporter_deliveryAttempts Delivery attempts per region
+# TYPE awsses_exporter_deliveryAttempts gauge
+awsses_exporter_deliveryAttempts{aws_region="us-east-1"} 5.0
+awsses_exporter_deliveryAttempts{aws_region="us-west-2"} 2.0
+# HELP awsses_exporter_rejects Rejects per region
+# TYPE awsses_exporter_rejects gauge
+awsses_exporter_rejects{aws_region="us-east-1"} 0.0
+awsses_exporter_rejects{aws_region="us-west-2"} 0.0
 ```
--- collector.go
+++ collector.go
@@ -1,51 +1,66 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/aws/aws-sdk-go/aws"
-
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
-	"gopkg.in/alecthomas/kingpin.v2"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 const (
 	namespace = "awsses_exporter"
 )
 
-// Exporter collects metrics from aws ses.
+// Exporter struct
 type Exporter struct {
 	accessKey       string
 	secretAccessKey string
 
-	max24hoursend   *prometheus.Desc
-	maxsendrate     *prometheus.Desc
-	sentlast24hours *prometheus.Desc
+	bounces          *prometheus.Desc
+	complaints       *prometheus.Desc
+	deliveryAttempts *prometheus.Desc
+	rejects          *prometheus.Desc
+	timestamp        *prometheus.Desc
 }
 
 // NewExporter returns an initialized exporter.
 func NewExporter() *Exporter {
 	return &Exporter{
-		max24hoursend: prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "", "max24hoursend"),
-			"The maximum number of emails allowed to be sent in a rolling 24 hours.",
+		bounces: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "bounces"),
+			"Bounces per region",
 			[]string{"aws_region"},
 			nil,
 		),
-		maxsendrate: prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "", "maxsendrate"),
-			"The maximum rate of emails allowed to be sent per second.",
+		complaints: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "complaints"),
+			"Complaints per region",
 			[]string{"aws_region"},
 			nil,
 		),
-		sentlast24hours: prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "", "sentlast24hours"),
-			"The number of emails sent in the last 24 hours.",
+		deliveryAttempts: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "deliveryAttempts"),
+			"Delivery attempts per region",
+			[]string{"aws_region"},
+			nil,
+		),
+		rejects: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "rejects"),
+			"Rejects per region",
+			[]string{"aws_region"},
+			nil,
+		),
+		timestamp: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "timestamp"),
+			"Timestamp per region",
 			[]string{"aws_region"},
 			nil,
 		),
@@ -55,36 +70,53 @@ func NewExporter() *Exporter {
 // Describe describes all the metrics exported by the memcached exporter. It
 // implements prometheus.Collector.
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- e.max24hoursend
-	ch <- e.maxsendrate
-	ch <- e.sentlast24hours
+	ch <- e.bounces
+	ch <- e.complaints
+	ch <- e.deliveryAttempts
+	ch <- e.rejects
+	ch <- e.timestamp
 }
 
 // Collect fetches the statistics from the configured memcached server, and
 // delivers them as Prometheus metrics. It implements prometheus.Collector.
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	awsSesRegions := []string{"us-east-1", "us-west-2", "eu-west-1"}
+	awsSesRegions := []string{"us-east-1", "us-west-2"} //, "eu-west-1"}
+	latest := make(map[string]*ses.SendDataPoint)
 	for _, regionName := range awsSesRegions {
 		svc := ses.New(session.New(), aws.NewConfig().WithRegion(regionName))
-		input := &ses.GetSendQuotaInput{}
+		input := &ses.GetSendStatisticsInput{}
 
-		result, err := svc.GetSendQuota(input)
+		result, err := svc.GetSendStatistics(input)
 		if err != nil {
 			if aerr, ok := err.(awserr.Error); ok {
 				switch aerr.Code() {
 				default:
-					log.Errorf("Failed to get sending quota from region %s: %s", regionName, aerr)
+					fmt.Println(aerr.Error())
 				}
 			} else {
-				log.Errorf("Failed to get sending quota from region %s: %s", regionName, err)
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
 			}
 			return
 		}
+		sort.Slice(result.SendDataPoints, func(i, j int) bool {
+			return result.SendDataPoints[i].Timestamp.Unix() > result.SendDataPoints[j].Timestamp.Unix()
+		})
+		if len(result.SendDataPoints) > 0 {
+			latest[regionName] = result.SendDataPoints[0]
+		}
+		//for _, value := range latest {
+		ch <- prometheus.MustNewConstMetric(e.bounces, prometheus.GaugeValue, float64(*latest[regionName].Bounces), regionName)
+		ch <- prometheus.MustNewConstMetric(e.complaints, prometheus.GaugeValue, float64(*latest[regionName].Complaints), regionName)
+		ch <- prometheus.MustNewConstMetric(e.deliveryAttempts, prometheus.GaugeValue, float64(*latest[regionName].DeliveryAttempts), regionName)
+		ch <- prometheus.MustNewConstMetric(e.rejects, prometheus.GaugeValue, float64(*latest[regionName].Rejects), regionName)
+		//ch <- prometheus.MustNewConstMetric(e.timestamp, prometheus.GaugeValue, float64(value.Timestamp.Unix()), regionName)
+		//}
 
-		ch <- prometheus.MustNewConstMetric(e.max24hoursend, prometheus.GaugeValue, *result.Max24HourSend, regionName)
-		ch <- prometheus.MustNewConstMetric(e.maxsendrate, prometheus.GaugeValue, *result.MaxSendRate, regionName)
-		ch <- prometheus.MustNewConstMetric(e.sentlast24hours, prometheus.GaugeValue, *result.SentLast24Hours, regionName)
+		//fmt.Printf("%v", latest)
 	}
+
 }
 
 func main() {
@@ -101,6 +133,7 @@ func main() {
 	log.Infoln("Build context", version.BuildContext())
 
 	prometheus.MustRegister(NewExporter())
+	fmt.Println("RESULTSSSSSS")
 
 	http.Handle(*metricsPath, prometheus.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
